97738c6faa5a8e2ff02848d7e04c115fe1a0755a
Mathias Dosé <mathias.dose@hotmail.se>
2012-09-21 12:12:35 +0200

    removed the application panel

--- gen/com/example/shutapp/R.java
+++ gen/com/example/shutapp/R.java
@@ -10,10 +10,13 @@ package com.example.shutapp;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int test=0x7f040000;
+    }
     public static final class dimen {
-        public static final int padding_large=0x7f040002;
-        public static final int padding_medium=0x7f040001;
-        public static final int padding_small=0x7f040000;
+        public static final int padding_large=0x7f050002;
+        public static final int padding_medium=0x7f050001;
+        public static final int padding_small=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
@@ -29,18 +32,18 @@ public final class R {
         public static final int ic_social_group=0x7f02000a;
     }
     public static final class id {
-        public static final int banner=0x7f080006;
-        public static final int button1=0x7f080007;
-        public static final int chat_button=0x7f080005;
-        public static final int iconFrame_2=0x7f080001;
-        public static final int iconFrame_3=0x7f080008;
-        public static final int iconFrame_4=0x7f080009;
-        public static final int map_button=0x7f080004;
-        public static final int menu_settings=0x7f08000b;
-        public static final int nearby_conversations_button=0x7f080002;
-        public static final int settingsList=0x7f08000a;
-        public static final int settings_button=0x7f080003;
-        public static final int textView1=0x7f080000;
+        public static final int banner=0x7f090006;
+        public static final int button1=0x7f090007;
+        public static final int chat_button=0x7f090005;
+        public static final int iconFrame_2=0x7f090001;
+        public static final int iconFrame_3=0x7f090008;
+        public static final int iconFrame_4=0x7f090009;
+        public static final int map_button=0x7f090004;
+        public static final int menu_settings=0x7f09000b;
+        public static final int nearby_conversations_button=0x7f090002;
+        public static final int settingsList=0x7f09000a;
+        public static final int settings_button=0x7f090003;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
@@ -50,27 +53,27 @@ public final class R {
         public static final int activity_settings=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_chat=0x7f070000;
-        public static final int activity_main=0x7f070001;
-        public static final int activity_map=0x7f070002;
-        public static final int activity_nearby_conversations=0x7f070003;
-        public static final int activity_settings=0x7f070004;
+        public static final int activity_chat=0x7f080000;
+        public static final int activity_main=0x7f080001;
+        public static final int activity_map=0x7f080002;
+        public static final int activity_nearby_conversations=0x7f080003;
+        public static final int activity_settings=0x7f080004;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int main_information_temp=0x7f050008;
-        public static final int menu_settings=0x7f050002;
-        public static final int redirect_from_main_button=0x7f050009;
-        public static final int text_at_map_activity=0x7f05000a;
-        public static final int title_activity_chat=0x7f050004;
-        public static final int title_activity_contacts=0x7f050005;
-        public static final int title_activity_main=0x7f050003;
-        public static final int title_activity_map=0x7f050006;
-        public static final int title_activity_nearby_conversations=0x7f050007;
-        public static final int title_activity_settings=0x7f05000b;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int main_information_temp=0x7f060008;
+        public static final int menu_settings=0x7f060002;
+        public static final int redirect_from_main_button=0x7f060009;
+        public static final int text_at_map_activity=0x7f06000a;
+        public static final int title_activity_chat=0x7f060004;
+        public static final int title_activity_contacts=0x7f060005;
+        public static final int title_activity_main=0x7f060003;
+        public static final int title_activity_map=0x7f060006;
+        public static final int title_activity_nearby_conversations=0x7f060007;
+        public static final int title_activity_settings=0x7f06000b;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
