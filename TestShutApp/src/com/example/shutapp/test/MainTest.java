7984e9265ff7431ad1cb350d56aca1b606bea2ce
Hampus Lilja <hampus.lilja@gmail.com>
2012-09-21 13:57:00 +0200

    prep to merge

---
+++ TestShutApp/src/com/example/shutapp/test/MainTest.java
@@ -0,0 +1,96 @@
+package com.example.shutapp.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.shutapp.*;
+
+import android.app.Instrumentation.ActivityMonitor;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	
+	private MainActivity mActivity;
+	
+	private Button mButton;
+	private TextView resultView;
+	private ActivityMonitor activityMonitor;
+	  
+	public MainTest() {
+		super("com.example.shutapp.MainActivity", MainActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		// register next activity that need to be monitored.
+		activityMonitor = getInstrumentation().addMonitor(NearbyConversationsActivity.class.getName(), null, false);
+
+	    mActivity = getActivity();
+
+	    mButton =
+	      (Button) mActivity.findViewById(
+	        com.example.shutapp.R.id.button1
+	      );
+	    
+	    resultView = (TextView) mActivity.findViewById(
+	    		com.example.shutapp.R.id.textView1); 
+		
+	}
+	
+	public void testPreConditions() {
+		assertNotNull("Can´t find the Button", mButton);
+		assertNotNull("Can´t find the Text View", resultView);
+	    assertEquals("Wrong text",
+	    		"This is the main activity, please press the button to redirect", resultView.getText());
+	}
+	
+	public void testRedirectButton() {
+		
+		mActivity.runOnUiThread(
+				new Runnable() {
+					public void run() {
+						mButton.requestFocus();
+						mButton.performClick();
+					}
+				});
+
+		wait(6);
+		
+		NearbyConversationsActivity nActivity = (NearbyConversationsActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5);
+		
+		// next activity is opened and captured.
+		assertNotNull("Could´t open Activity", nActivity);
+		nActivity.finish();
+
+		resultView = (TextView) nActivity.findViewById(
+	    		com.example.shutapp.R.id.textView1); 
+		
+		assertNotNull("Can´t find the Text View", resultView);
+	    assertEquals("Wrong text",
+	    		"NearbyConversationsActivity", resultView.getText());
+	
+	}
+	
+	public void wait(int sec){
+		
+		try {
+			Thread.sleep(sec*1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	/*@Override
+	protected void tearDown() {
+		try {
+			super.tearDown();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}*/
+
+}
