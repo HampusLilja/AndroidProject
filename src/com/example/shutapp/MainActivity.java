e189acb4bc76cbd8cb73e36785cce1ca0cdcb606
Mathias Dosé <mathias.dose@hotmail.se>
2012-09-21 15:11:00 +0200

    displays account

--- src/com/example/shutapp/MainActivity.java
+++ src/com/example/shutapp/MainActivity.java
@@ -1,18 +1,70 @@
 package com.example.shutapp;
 
 import android.app.Activity;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import android.*;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manageAccount();
+    }
+    
+    public void manageAccount(){
+    	AccountManager am = AccountManager.get(this);
+    	Bundle options = new Bundle();
+    	
+    	Account[] accounts = am.getAccountsByType("com.google");
+    	List<String> possibleEmails = new LinkedList<String>();
+    	
+    	//this is just for testing, the user should be able to choose what gmail acc he wants to use
+    	Account theAcc = accounts[0];
+    	
+   /* 	am.getAuthToken(
+    		    theAcc,                         // Account retrieved using getAccountsByType()
+    		    "Manage your tasks",            // Auth scope
+    		    options,                        // Authenticator-specific options
+    		    this,                           // Your activity
+    		    new OnTokenAcquired(),          // Callback called when a token is successfully acquired
+    		    new Handler(new OnError()));    // Callback called if an error occurs
+    	
+    
+    	*/
+    	
+    	
+    	TextView textview = (TextView) findViewById(R.id.textView1);
+    	textview.setText(theAcc.name);
     }
 
     public void redirectFromMain(View view){
@@ -20,5 +72,6 @@ public class MainActivity extends Activity {
     	startActivity(intentToRedirect);
     	overridePendingTransition(0, 0);
     }
+    
     
 }
