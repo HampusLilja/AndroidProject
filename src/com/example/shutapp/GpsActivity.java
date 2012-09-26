8b8c76cd9d775671a1346967a9ddee8c624eb546
Hampus Lilja <hampus.lilja@gmail.com>
2012-09-26 21:13:00 +0200

    positioning should work now

--- src/com/example/shutapp/GpsActivity.java
+++ src/com/example/shutapp/GpsActivity.java
@@ -3,6 +3,7 @@ package com.example.shutapp;
 import java.util.List;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.content.Context;
@@ -14,33 +15,39 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
 
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 
-public class GpsActivity extends MapActivity {
+public class GpsActivity extends MapActivity implements LocationListener {
 	private final int zoomLevel = 17; // 1 to 21
 	private GeoPoint gP;
+	//private LocationManager lm;
+	private MapController mControl;
+	private double Latitude = 57.7012596130371;
+	private double Longitude = 11.9670495986938;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1, this);
+      
         MapView mapView = (MapView) findViewById(R.id.mapView);
         
         mapView.setBuiltInZoomControls(true);   //true = being able to zoom with buttons
         mapView.getController().setZoom(zoomLevel); //sets zoomlevel from the startup
     
-        final MapController mControl = mapView.getController();
-        LocationManager locManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        
-     // TEMP BELOW (must have gp-value here or else FATAL?)
-        double lon = 11.9670495986938;
-        double lat = 57.7012596130371;
-        gP = new GeoPoint((int)(lat*1E6), (int)(lon*1E6)); //converting to micro-degrees with 1E6
-        mControl.animateTo(gP);
-     // End Temp
+        mControl = mapView.getController();
+
+        Location startLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double startLongitude = startLocation.getLongitude();
+        double startLatitude = startLocation.getLatitude();
+        updatePosition(startLatitude, startLongitude);
         
         List<Overlay> mapOverlays = mapView.getOverlays();
         Drawable drawable = this.getResources().getDrawable(R.drawable.maparrow);
@@ -49,31 +56,31 @@ public class GpsActivity extends MapActivity {
         OverlayItem overlayitem = new OverlayItem(gP, null, null);
         itemizedoverlay.addOverlay(overlayitem);
         mapOverlays.add(itemizedoverlay);
-        LocationListener locList = new LocationListener(){
+        
+    }
+    
+    private void updatePosition(double latitude, double longitude) {
+    	gP = new GeoPoint((int)(latitude*1E6), (int)(longitude*1E6)); //converting to micro-degrees with 1E6
+        mControl.animateTo(gP);
+	}
 
-			public void onLocationChanged(Location location) {
-				gP = new GeoPoint((int)location.getLatitude(), (int)location.getLongitude());
-        		mControl.animateTo(gP); //animateTo or setCenter
-				
-			}
-
-			public void onProviderDisabled(String provider) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			public void onProviderEnabled(String provider) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			public void onStatusChanged(String provider, int status,
-					Bundle extras) {
-				// TODO Auto-generated method stub
-				
-			}
-        };
-        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locList); //TEMP! 0 = min.time ,0 = min.distance
+	public void onLocationChanged(Location arg0) {
+    	double latitude = arg0.getLatitude();
+        double longitude = arg0.getLongitude();
+    	String lat = String.valueOf(latitude);
+        String lon = String.valueOf(longitude);
+        Log.e("GPS", "location changed: lat="+lat+", lon="+lon);
+        updatePosition(latitude, longitude);
+    }
+    
+    public void onProviderDisabled(String arg0) {
+        Log.e("GPS", "provider disabled " + arg0);
+    }
+    public void onProviderEnabled(String arg0) {
+        Log.e("GPS", "provider enabled " + arg0);
+    }
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+        Log.e("GPS", "status changed to " + arg0 + " [" + arg1 + "]");
     }
     
     @Override
