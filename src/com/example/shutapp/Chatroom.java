bb537a207c3cb442ffc50f2dc09d7d9946f8783c
Prassel123 <Prassel123@gmail.com>
2012-09-27 10:46:14 +0200

    no comment

--- src/com/example/shutapp/Chatroom.java
+++ src/com/example/shutapp/Chatroom.java
@@ -1,5 +1,20 @@
 package com.example.shutapp;
 
-public class Chatroom {
+import java.util.List;
 
+import android.location.Location;
+
+public class Chatroom {
+	
+	private List<String> members;
+	private Location gps_location;
+	private final String CHATROOM_NAME;
+	
+	public Chatroom (String name){
+		CHATROOM_NAME = name;
+		
+	}
+	
+	
+			
 }
