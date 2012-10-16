88af9bbfb5db727fd7057cc8ede41388fc429706
tomasar <tomas.arnesson@gmail.com>
2012-10-17 01:07:29 +0200

    Updated server-package. Refactor, added copyright info. Added removeUserFromChatRoom function, rewritten addUserToChatRoom function for correct implementation for database. Fixed bugs regarding database link. Rewritten algoritm for adding multiple regIds to same chatroom. Moved GCM package within the server-package.

--- server-package/README.txt
+++ server-package/README.txt
@@ -1,4 +1,4 @@
-
+ * ShutApp Server-Package v.3
  * This is a added server package for ShutAPP Android application. Created in Netbeans IDE 7.2
  * Copyright (C) 2012  Hampus Lilja, Tomas Arnesson, Mathias Dosé, Mathias Herzog
  * This program uses GNU General Public License, please read supplied LICENCE.TXT for more info.
@@ -8,4 +8,4 @@ For installation, please Import Project into Netbeans and build/deploy.
 This updated version now uses SQLite Android native database instead of Derby.
 For usage make sure you have org.sqlite.JDBC driver installed and added to dependency in Netbeans.
 Also for now the path to shutappdb.db is static, so make sure its valid in top of GCM_Server java class.
-Make sure you have all included libraries added in Netbeans project, for example com.google.android.gcm.server.
+Gcm Server package now included in com.google.android.gcm.
--- server-package/ShutAPP_GCM_SERVER/src/main/java/com/tomasar/shutapp_gcm_server/GCM_Server.java
+++ server-package/ShutAPP_GCM_SERVER/src/main/java/com/tomasar/shutapp_gcm_server/GCM_Server.java
@@ -7,21 +7,22 @@
  ** A copy of the license is included in the section entitled "LICENSE.txt".
  */
 /*
-** This file is part of ShutApp.
-**
-** ShutApp is free software: you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation, either version 3 of the License, or
-** (at your option) any later version.
-**
-** ShutApp is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with ShutApp.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ ** This file is part of ShutApp.
+ **
+ ** ShutApp is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** ShutApp is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with ShutApp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.tomasar.shutapp_gcm_server;
 
 import com.google.android.gcm.server.*;
@@ -51,7 +52,6 @@ import org.sqlite.SQLiteOpenMode;
 /*
  * This is added server package to ShutAPP Android application.
  * Copyright (C) 2012  Hampus Lilja, Tomas Arnesson, Mathias Dosé, Mathias Herzog
- * This program uses GNU General Public License, please read supplied LICENCE.TXT for more info.
  */
 
 @WebServlet(name = "GCM", urlPatterns = {"/GCM"})
@@ -60,29 +60,21 @@ public class GCM_Server extends HttpServlet implements Serializable {
     //This is the registered key for access via Google API, GCM.
     private static final String SENDER_ID = "AIzaSyAbgoOa_EvIBk81TxI8oTm8d_0he1aqzWU";
     private static final String SENDER_ID2_browser = "AIzaSyAGbXeI9lkNaqDp_oFove5dmUNqEsD5FOA";
-    
-    //These are static configurations, for testing purposes.
-    private static final String TOMAS = "APA91bEqKVrHg19T-I5MKgp707wMhcgAdmzOfr0IPAsTyk5O0citkgjl4rHdYcz7axKbA9ODwDFPb4I2ISwwdZntheJcLdoeHJCkESs7F145PKBmU92bALDPg_ClwS82pO3UnLVGCvmAyWl15B9Qpk9jjQd-Fwa_5Q";
-    private static final String DOSE = "APA91bHCMst1WyecOE-T6IyrLlwa2E-9mwRYVE3T1KHrdcFNMBlehvxmHHPWNGPL0alH02cfOxuj1Qvk4rc03k3Zd0Khp33d4dREYUSXUGQ_nO1Vdkhg03Qg4j4jsojmSk89Ym4AlsCMuglHHU47TuX4SbJNYSONFg";     
     /*
-     * Static SQL part/strings below this line.
-     */
-    //MYSQL part containing vital connect information.
-    //private static String DBURL = "jdbc:derby://localhost:1527/SHUTAPPDB";
+     * !-!-! BEFORE DEPLOYING CHECK PATH BELOW !-!-!
+     * Static path to server SQLite database below
+    */
     private static String DBURL = "jdbc:sqlite:C:\\Tomas\\Software Engineering\\ShutAPP_GCM_SERVER\\src\\main\\webapp\\shutappdb.db";
     private static String DBUSER = "gcm";
     private static String DBPASS = "gcm";
-    //private static String DBDriver = "org.apache.derby.jdbc.ClientDriver";
     private static String DBDriver = "org.sqlite.JDBC"; 
-    //MYSQL prepared statements
-    //When a new users is registered, put hes/hers chose nickname in DB
-    private static String newUserToSQLDB = "INSERT INTO USERS" + "(REGID, NICK) VALUES" + "(?,?)";
-    //When a new chatroom is created, put the details into DB
+    /*
+     * MYSQL prepared statements below:
+     */
+     //When a new chatroom is created, put the details into DB
     private static String createNewChatRoomInDB = "INSERT INTO CHATROOMS " + "(NAME, MEMBERS, LAT, LONG, RADIUS, PASSWD) VALUES " + "(?,?,?,?,?,?)";
-    private static String addMemberToChatRoomInDB = "UPDATE CHATROOMS SET MEMBERS=" + "?" + "  WHERE NAME=" + "?";
-    //When a user uninstall application, remove the REGID from GCM.USERS
+    private static String updateMemberListForChatRoomInDB = "UPDATE CHATROOMS SET MEMBERS=" + "?" + "  WHERE NAME=" + "?";
     private static String delUserFromSQLDB = "DELETE FROM USERS WHERE REGID" + "=" + "'" + "(?)" + "'";
-    //Checks which users are members or chatroom
     private static String getCurrentChatRoomUsers = "SELECT MEMBERS FROM CHATROOMS WHERE NAME="+"?";
     
     public GCM_Server() {
@@ -98,13 +90,11 @@ public class GCM_Server extends HttpServlet implements Serializable {
      * Processes requests for both HTTP
      * <code>GET</code> and
      * <code>POST</code> methods.
-     * 
-     * Not implemented, currently only POST is being used.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * NOTE: Not implemented, currently only POST is being used.
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -120,6 +110,7 @@ public class GCM_Server extends HttpServlet implements Serializable {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * NOTE: Not implemented, currently only POST is being used.
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -142,10 +133,11 @@ public class GCM_Server extends HttpServlet implements Serializable {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
         String typeOfAction = request.getParameter("action");
         //response.setContentType("text/html;charset=UTF-8");
-        //response.setContentType("application/json");
+         /*
+         * This ACTION handles sending GCM message to specific chatroom.
+         */ 
             if( typeOfAction.equals("chatRoomMsg")) {
         String chatRoom = request.getParameter("chatRoom");
         String userMessage = request.getParameter("Message");
@@ -159,35 +151,34 @@ public class GCM_Server extends HttpServlet implements Serializable {
          * This ACTION handles creation of a new chatroom.
          */ 
         String name = request.getParameter("NAME");
-        String members = request.getParameter("MEMBERS");
+        String members = "";
         String lat = request.getParameter("LAT");
         String longitud = request.getParameter("LONG");
         String radius = request.getParameter("RADIUS");
         String passwd = request.getParameter("PASSWD");
-        //applyChangesToSQL("roomtest", usertest, lat, longitud, radius, createNewChatRoomInDB);
         applyChangesToSQL(name, members, lat, longitud, radius, passwd, createNewChatRoomInDB);
         Logger.getAnonymousLogger().log(Level.INFO, "Recieved HTTP POST for creation of new chatroom.");
         //line below is only for test environment.
         response.sendRedirect("index.jspx");
-       }else if( typeOfAction.equals("removeChatroom")) {
+       }else if( typeOfAction.equals("leaveChatroom")) {
         /*
          * This ACTION handles the removal of a user from a chatroom.
          */  
         String chatRoom = request.getParameter("name");
         String regId = request.getParameter("regid");
-        //removeUserFromChatRoom(DBREGID, delUserFromSQLDB);
-        Logger.getAnonymousLogger().log(Level.INFO, "Recieved HTTP POST for new user to join chatroom: " + chatRoom);
+        applyChangesToSQL(removeUserFromChatRoom(regId, chatRoom), chatRoom, updateMemberListForChatRoomInDB);
+        Logger.getAnonymousLogger().log(Level.INFO, "Recieved HTTP POST for new user to leave chatroom: " + chatRoom);
         //line below is only for test environment.
         response.sendRedirect("index.jspx");
         }else if( typeOfAction.equals("joinChatroom")) {
+            
         /*
-         * This ACTION handles adding a new member to a chatroom(join).
+         * This ACTION handles joining a chatroom.
          */  
         String chatRoom = request.getParameter("name");
         String regId = request.getParameter("regid");
-        applyChangesToSQL(addUserToChatRoom(regId, chatRoom), chatRoom, addMemberToChatRoomInDB);
-        
-        Logger.getAnonymousLogger().log(Level.INFO, "Recieved HTTP POST for new user to join chatroom: " + chatRoom);
+        Logger.getAnonymousLogger().log(Level.INFO, "Recieved HTTP POST for:" +regId +" to join chatroom: " + chatRoom);
+        applyChangesToSQL(addUserToChatRoom(regId, chatRoom), chatRoom, updateMemberListForChatRoomInDB);
         //line below is only for test environment.
         response.sendRedirect("index.jspx");
         }
@@ -196,7 +187,6 @@ public class GCM_Server extends HttpServlet implements Serializable {
 
     /**
      * Returns a short description of the servlet.
-     *
      */
     @Override
     public String getServletInfo() {
@@ -219,16 +209,7 @@ public class GCM_Server extends HttpServlet implements Serializable {
         // Could be either string or JSON Object!
         Message message;
         message = new Message.Builder()
-
-// If multiple messages are sent using the same .collapseKey()
-// the android target device, if it was offline during earlier message
-// transmissions, will only receive the latest message for that key when
-// it goes back on-line.
-//.collapseKey(chatRoom)
-//.timeToLive(30)
-//.delayWhileIdle(true)
-// Above functions not implemented in ShutApp
-                
+               
 .addData("chatroom", chatRoom)
 .addData("message", temp.toString())
 .build();
@@ -239,11 +220,11 @@ public class GCM_Server extends HttpServlet implements Serializable {
     
     /*
      * The following function is responsible for sending a built message to GCM server
-     * imparmeters is a formatted and built message, by com.google.android.gcm.server.message; class
+     * inparameters are a formatted and built message, by com.google.android.gcm.server.message; class.
+     * And an arraylist of regIds of devices to send to.
      */
     public void sendMsgToChatRoom(Message message, List<String> users){
-                        // Instance of com.android.gcm.server.Sender, that does the
-                // transmission of a Message to the Google Cloud Messaging service.
+    //Instance of com.android.gcm.server.Sender, that does the, transmission of a Message to GCM.
         Sender sender = new Sender(SENDER_ID);
         try {
             
@@ -375,7 +356,7 @@ public class GCM_Server extends HttpServlet implements Serializable {
     
     
     
-            /*
+     /*
      * This class handles updates to SQL Database, which has five variables.
      * And uses predefined static SQL strings, with variables.
      */
@@ -393,7 +374,7 @@ public class GCM_Server extends HttpServlet implements Serializable {
         ps_sql.setString(5, var5);
         ps_sql.setString(6, var6);
         ps_sql.executeUpdate();
-        System.out.println("JAVA: Performed sql string " + sqlstatement + " with variables " + var1 +" " + var2 +" " + var3 +" " +var4 +" " +var5 +" " +var6);
+        System.out.println("JAVA: Performed sql string " + sqlstatement + " with variables " + var1 +" " + var2 +" " + var3 +" " +var4 +" " +var5);
       }
       catch(SQLException e){
           System.out.println(e.getMessage());
@@ -423,7 +404,7 @@ public class GCM_Server extends HttpServlet implements Serializable {
     
         /*
          * Collects a list of members for a specific chatroom, splits it to regIds and adds into an arraylist.
-         * It then returns the arraylist.
+         * It then returns the arraylist. With a string for the name of the chatRoom as inparameter.
          * */
          
         private static List<String> convertToArrayListForGCM(String chatRoom){
@@ -442,7 +423,7 @@ public class GCM_Server extends HttpServlet implements Serializable {
         while (result.next()) {               // Position the cursor                   
         members = result.getString(1);        // Retrieve the first column value
         chatroom_members = new ArrayList<String>(Arrays.asList(members.split(",")));
-        //chatroom_members.add(members);
+        
         result.close();
         
 }
@@ -473,6 +454,9 @@ public class GCM_Server extends HttpServlet implements Serializable {
         }
       return chatroom_members;
         }
+        /*
+         * Checks current members in a chatRoom and adds a new one to the list.
+         */
         
         private String addUserToChatRoom(String regId, String chatRoom){
         List<String> chatroom_members = new ArrayList<>();
@@ -494,11 +478,83 @@ public class GCM_Server extends HttpServlet implements Serializable {
         ps_sql.close();
         /* ok now we have our info, append and update with new*/
         StringBuffer temp = new StringBuffer();
+        if( members.equals("")){
+            temp.append(regId);
+            updated_memberlist = temp.toString();
+            applyChangesToSQL(updated_memberlist, chatRoom, updateMemberListForChatRoomInDB);
+        }else{
         temp.append(members);
         temp.append(",");
         temp.append(regId);
         updated_memberlist = temp.toString();
-        applyChangesToSQL(updated_memberlist, chatRoom, addMemberToChatRoomInDB);
+        applyChangesToSQL(updated_memberlist, chatRoom, updateMemberListForChatRoomInDB);
+        }
+}
+      }
+      catch(SQLException e){
+          System.out.println(e.getMessage());
+      }		
+          catch (Exception e){
+        e.printStackTrace();
+    }
+      finally {
+            if (ps_sql != null) {
+                         try{
+                        	ps_sql.close();
+                            }
+                           catch(SQLException e){
+                           System.out.println(e.getMessage());
+            }	
+                         try{
+                            if (SQLconnection != null) {
+				SQLconnection.close();
+                            }
+            }
+                            catch(SQLException e){
+                           System.out.println(e.getMessage());
+                            }	
+            }
+
+        }return updated_memberlist;
+        }
+
+
+        /*
+         * Connects to database and removes a users device regId from column MEMBERS.
+         * Input parameters are what regId and which chatRoom.
+         */
+        private String removeUserFromChatRoom(String regId, String chatRoom){
+        List<String> chatroom_members = new ArrayList<>();
+        Connection SQLconnection = null;
+        PreparedStatement ps_sql = null;
+        ResultSet result;
+        String members;
+        String updated_memberlist = "";
+      try{
+        SQLconnection = connectSQLDB();
+        
+        ps_sql = SQLconnection.prepareStatement(getCurrentChatRoomUsers);
+        ps_sql.setString(1, chatRoom);
+        result = ps_sql.executeQuery();
+        System.out.println("JAVA: Performed sql string " + getCurrentChatRoomUsers+ "with variables " + chatRoom);
+        while (result.next()) {               // Position the cursor                   
+        members = result.getString(1);        // Retrieve the first column value
+        result.close();
+        ps_sql.close();
+        /* ok now we have our info, append and update with new*/
+          String[] temp;
+          StringBuffer sb= new StringBuffer();;
+          temp = members.split(",");
+          for(int i = 0; i<temp.length; i++){
+            if(!temp[i].equals(regId)){
+            sb.append(temp[i]);
+            if(i != temp.length-2){
+            sb.append(",");
+                }
+            }
+        }
+        updated_memberlist = sb.toString();
+        applyChangesToSQL(updated_memberlist, chatRoom, updateMemberListForChatRoomInDB);
        
 }
       }
@@ -529,4 +585,3 @@ public class GCM_Server extends HttpServlet implements Serializable {
         }return updated_memberlist;
         }
 }
-
